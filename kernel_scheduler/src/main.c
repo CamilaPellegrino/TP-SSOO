97c953fe6fd3c78a3b9034dd391e2125dd56169d
Camila <cpellegrino@frba.utn.edu.ar>
2026-05-05 20:35:47 +0000

    mover estructuras de utils a cada modulo

--- kernel_scheduler/src/main.c
+++ kernel_scheduler/src/main.c
@@ -1,5 +1,6 @@
 #include <utils/hello.h>
 #include <utils/utils.h>
+#include "base_sch.h"
 
 void* atender_cliente(void *arg);
 void atender_cpu(t_cpu* cpu);
---
+++ swap/src/base_swap.h
@@ -0,0 +1,54 @@
+#ifndef BASE_SWAP_H_
+#define BASE_SWAP_H_
+
+#include <utils/utils.h>
+typedef struct 
+{
+    int tamanio;
+    int fd;
+    char* puerto;
+    char* ip;
+}t_stick;
+
+typedef struct
+{
+	int fd;
+	int id;
+} t_cpu;
+
+typedef struct
+{
+	int fd;
+	t_tipo_io tipo;
+} t_io;
+
+typedef struct
+{
+	// identificadores del proceso
+	int pid;
+	int ppid;
+	int priodidad;
+	t_tipo_estado estado;
+	// registros de estado
+	uint32_t pc;
+	uint8_t ax;
+	uint8_t bx;
+	uint8_t cx;
+	uint8_t dx;
+	uint32_t eax;
+	uint32_t ebx;
+	uint32_t ecx;
+	uint32_t edx;
+	uint32_t si;
+	uint32_t di;
+	
+} t_pcb;
+
+
+t_stick* iniciar_stick(char* ip, char* puerto, int tamanio, int cliente_fd);
+void destruir_stick(t_stick* stick);
+t_cpu* iniciar_cpu(int id, int fd);
+t_io* iniciar_io(t_tipo_io tipo_io, int io_fd);
+
+
+#endif
